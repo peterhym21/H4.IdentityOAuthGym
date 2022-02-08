6f0f467c95d09a064a6261554f1dd1f197f07459
Peter Hymøller <pete102c@elevcampus.dk>
2022-02-08 13:07:36 +0100

    APIKey and conole

---
+++ CallAPiAuthorized/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Http.Json;
+using APIKey;
+
+HttpClient Http = new();
+
+WeatherForecast weatherForecast = await Http.GetFromJsonAsync<WeatherForecast>("https://localhost:7052/WeatherForecast?apikey=12345");
+
+Console.WriteLine(weatherForecast.Date);
+Console.WriteLine(weatherForecast.Summary);
+Console.WriteLine(weatherForecast.TemperatureC);
+Console.WriteLine(weatherForecast.TemperatureF);
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,34 @@
+using APIKey.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = ApiKeyAuthOptions.DefaultScheme;
+    options.DefaultChallengeScheme = ApiKeyAuthOptions.DefaultScheme;
+}).AddApiKeyAuth(builder.Configuration.GetSection("Authentication").Bind);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
