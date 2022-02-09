7ea812b5672730a784f59d3a6b0a5fa6f77a1ab3
Peter Hymøller <pete102c@elevcampus.dk>
2022-02-09 08:18:35 +0100

    update

--- CallAPiAuthorized/Program.cs
+++ CallAPiAuthorized/Program.cs
@@ -1,21 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
 using APIKey;
 
 HttpClient Http = new();
-
 List<WeatherForecast> weatherForecast = await Http.GetFromJsonAsync<List<WeatherForecast>>(requestUri: "https://localhost:7052/WeatherForecast?apikey=12345");
-
 
 foreach (var itemForecast in weatherForecast)
 {
-    Console.WriteLine(itemForecast.Date);
-    Console.WriteLine(itemForecast.Summary);
-    Console.WriteLine(itemForecast.TemperatureC);
-    Console.WriteLine(itemForecast.TemperatureF);
+    Console.WriteLine("-------------------------------------------------------");
+    Console.WriteLine($"Date: {itemForecast.Date}");
+    Console.WriteLine($"Summary: {itemForecast.Summary}");
+    Console.WriteLine($"Temp C: {itemForecast.TemperatureC}");
+    Console.WriteLine($"Temp F: {itemForecast.TemperatureF}");
 }
-
-
 Console.ReadLine();
